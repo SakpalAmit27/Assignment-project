ab3b9c386ee0d288304188369784e756c95d1a01
Sakpal Amit <145037496+SakpalAmit27@users.noreply.github.com>
2024-02-18 11:19:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let slideIndex = 0;
+let slides = document.querySelectorAll('.slide');
+let autoPlayInterval;
+
+function showSlide(index) {
+  if (index < 0) {
+    slideIndex = slides.length - 1;
+  } else if (index >= slides.length) {
+    slideIndex = 0;
+  }
+
+  document.querySelector('.slider').style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+function changeSlide(direction) {
+  showSlide(slideIndex += direction);
+}
+
+function togglePlayPause() {
+  let playPauseButton = document.querySelector('.play-pause-btn');
+  if (playPauseButton.innerHTML === 'Pause') {
+    playPauseButton.innerHTML = 'Play';
+    clearInterval(autoPlayInterval);
+  } else {
+    playPauseButton.innerHTML = 'Pause';
+    autoPlayInterval = setInterval(() => changeSlide(1), 2000); // Change slide every 2 seconds
+  }
+}
+
+// Start auto play
+togglePlayPause();
